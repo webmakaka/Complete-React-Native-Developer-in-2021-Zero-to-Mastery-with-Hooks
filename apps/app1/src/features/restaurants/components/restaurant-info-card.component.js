9bc47f1e8e76247e90077846216f84337816734e
WebMakaka <marley.nodejs@gmail.com>
2021-03-13 09:16:07 +0300

    7-Master Project - MealsToGo Core Part 1 | 102-Solution Migrating To Styled Components

--- apps/app1/src/features/restaurants/components/restaurant-info-card.component.js
+++ apps/app1/src/features/restaurants/components/restaurant-info-card.component.js
@@ -1,10 +1,20 @@
 import React from 'react';
-import { StyleSheet } from 'react-native';
+import { StyleSheet, Text } from 'react-native';
 import { Card } from 'react-native-paper';
 import styled from 'styled-components/native';
 
-const Title = styled.Text`
+const RestaurantCard = styled(Card)`
+  background-color: white;
+`;
+
+const RestaurantCardCover = styled(Card.Cover)`
+  padding: 20px;
+  background-color: white;
+`;
+
+const Title = styled(Text)`
   padding: 16px;
+  color: red;
 `;
 
 export const RestaurantInfoCard = ({ restaurant = {} }) => {
@@ -19,10 +29,10 @@ export const RestaurantInfoCard = ({ restaurant = {} }) => {
   } = restaurant;
 
   return (
-    <Card elevation={5} style={styles.card}>
-      <Card.Cover key={name} style={styles.cover} source={{ uri: photos[0] }} />
+    <RestaurantCard elevation={5}>
+      <RestaurantCardCover key={name} source={{ uri: photos[0] }} />
       <Title>{name}</Title>
-    </Card>
+    </RestaurantCard>
   );
 };
 
